d0d544d780727f577acb4bf11bc61b708e2db9e2
clenn <acrivopoulosdotcom@gmail.com>
2023-08-30 15:23:26 +0200

    delete unnecessary code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css'
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import NewEntryLogbookPage from "./NewEntryLogbookPage/NewEntryLogbookPage.tsx";
+import NewEntryLogbookSite from "./NewEntryLogbookSite/NewEntryLogbookSite.tsx";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
+import LogbookSite from "./LogbookSite/LogbookSite.tsx";
+import CustomerSearchSite from "./CustomerSearchSite/CustomerSearchSite.tsx";
+import CustomerFileSite from "./CustomerFileSite/CustomerFileSite.tsx";
+import ChooseCarSite from "./ChooseCarSite/ChooseCarSite.tsx";
+import LoginSite from "./LoginSite/LoginSite.tsx";
+import RegisterSite from "./RegisterSite/RegisterSite.tsx";
 
 function App() {
 
@@ -11,7 +15,13 @@ function App() {
     <>
       <Router>
           <Routes>
-              <Route path="/" element={<NewEntryLogbookPage />} />
+              <Route path="/login" element={<LoginSite />} />
+              <Route path="/register" element={<RegisterSite />} />
+              <Route path="/" element={<ChooseCarSite />} />
+              <Route path="/logbook" element={<LogbookSite />} />
+              <Route path="/newentry" element={<NewEntryLogbookSite />} />
+              <Route path="/customersearch" element={<CustomerSearchSite />} />
+              <Route path="/customerfile" element={<CustomerFileSite />} />
           </Routes>
       </Router>
     </>
