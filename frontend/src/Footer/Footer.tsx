a65fbfe1f6d672e343b45644c90861347e33389b
clenn <acrivopoulosdotcom@gmail.com>
2023-09-04 11:15:13 +0200

    implement isHovered- and isActive-Status

--- frontend/src/Footer/Footer.tsx
+++ frontend/src/Footer/Footer.tsx
@@ -1,14 +1,24 @@
 import Container from "react-bootstrap/Container";
 import './Footer.css'
 import {Image} from "react-bootstrap";
+import {useState} from "react";
 
 
 export default function Footer() {
+    const [isHovered, setIsHovered] = useState(false);
+    const [isActive, setIsActive] = useState(false);
     return (
         <>
-            <Container className="footer">
-                <a href="#header">
-                    <Image className="footer-icon" src="/icons/Icon small.png" rounded fluid />
+            <Container className="footer"
+
+            >
+                <a href="#header"
+                   onMouseEnter={() => setIsHovered(true)}
+                   onMouseLeave={() => setIsHovered(false)}
+                   onTouchStart={() => setIsActive(true)}
+                   onTouchEnd={() => setIsActive(false)}
+                >
+                    <Image className="footer-icon" src={isHovered || isActive ? "./icons/red-card.png" : "./icons/blue-card.png"} rounded fluid />
                 </a>
             </Container>
         </>
