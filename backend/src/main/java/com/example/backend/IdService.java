f46c91176b86ab129834d40d627b110ae98f0c9a
clenn <acrivopoulosdotcom@gmail.com>
2023-09-05 17:48:39 +0200

    implement IdService;

---
+++ backend/src/main/java/com/example/backend/IdService.java
@@ -0,0 +1,11 @@
+package com.example.backend;
+
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class IdService {
+
+    public String generateID() { return UUID.randomUUID().toString();}
+}
