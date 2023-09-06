7698f06d04a1a2ed0e0783a109934797988aceff
clenn <acrivopoulosdotcom@gmail.com>
2023-09-06 18:06:24 +0200

    add constants, onChangeHandler(), login() and axios

--- frontend/src/LoginSite/LoginSite.tsx
+++ frontend/src/LoginSite/LoginSite.tsx
@@ -1,9 +1,32 @@
 import "./Login.css";
 import {Card, Form, Image} from "react-bootstrap";
 import Header from "../Header/Header.tsx";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import {ChangeEvent, FormEvent, useState} from "react";
+import axios from "axios";
 
 export default function LoginSite() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const nav = useNavigate();
+
+    function onChangeHandlerEmail(event: ChangeEvent<HTMLInputElement>) {
+        setEmail(event.target.value);
+    }
+
+    function onChangeHandlerPassword(event: ChangeEvent<HTMLInputElement>) {
+        setPassword(event.target.value);
+    }
+
+    function login(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault()
+        axios.post("/api/user/login", {email, password})
+            .then(() => nav("/"))
+            .then(() => console.log("es hat ohne Security geklappt"))
+            .catch((error) => console.log(error))
+    }
+
     return (
         <>
             <Card className="wrapper">
@@ -11,9 +34,9 @@ export default function LoginSite() {
                 <div className="card-body" style={{ width: '18rem' }}>
                     <Image className="main-logo" src="./images/Logo.png" fluid />
                     <h3 className="main-h3">LOGIN</h3>
-                    <Form className="form-area">
-                        <input type="email" placeholder="Deine E-Mail-Adresse"></input>
-                        <input type="password" placeholder="Dein Password"></input>
+                    <Form className="form-area" onSubmit={login}>
+                        <input type="email" placeholder="Deine E-Mail-Adresse" onChange={onChangeHandlerEmail}></input>
+                        <input type="password" placeholder="Dein Password" onChange={onChangeHandlerPassword}></input>
                         <button className="btn-fullwidth main-btn">LOGIN</button>
                     </Form>
 
