0ae5c8f8c46d20780f58296efd9d8f46c38f8687
clenn <acrivopoulosdotcom@gmail.com>
2023-09-12 09:26:15 +0200

    replace CarOverview against LabelOverview data package

---
+++ frontend/src/LabelOverviewSite/LabelOverviewSite.tsx
@@ -0,0 +1,86 @@
+import Header from "../Header/Header.tsx";
+import "./LabelOverview.css";
+import {Button, Card, Image} from "react-bootstrap";
+import Footer from "../Footer/Footer.tsx";
+import {Link, useNavigate} from "react-router-dom";
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+type Label = {
+    id: string,
+    userId: string,
+    name: string,
+}
+
+export default function LabelOverviewSite() {
+    const [labels, setLabels] = useState<Label[]>([])
+    const [refreshData, setRefreshData] = useState(false);
+    const userId = 1;
+    const nav = useNavigate()
+
+    function deleteLabel(labelId) {
+        axios.delete("/api/label/deleteLabel/" +labelId)
+            .then(() => {
+                console.log(labelId);
+                setRefreshData(!refreshData);
+                nav("/labelOverview");
+            })
+            .catch((error) => console.log(error));
+    }
+
+    useEffect(() => {
+        if (refreshData) {
+            axios({
+                method: 'get',
+                url: '/api/label/getAllLabels/' + userId,
+            })
+                .then(function (response) {
+                console.log("Response Status: ", response.status);
+                console.log("Response Body: ", response.data);
+                setLabels(response.data);
+            })
+            .finally(() => {
+                setRefreshData(false);
+            });
+        }
+    }, [userId, refreshData]);
+
+    useEffect(() => {
+            axios({
+                method: 'get',
+                url: '/api/label/getAllLabels/' + userId,
+            })
+                .then(function (response) {
+                    console.log("Response Status: ", response.status);
+                    console.log("Response Body: ", response.data);
+                    setLabels(response.data);
+                })
+    }, [userId]);
+
+    return (
+        <>
+            {labels === undefined ? (
+                <h2>LOADING...</h2>
+            ) : (
+            <Card className="wrapper">
+                <Header />
+                <Card.Title className="card-title">
+                    <Image className="site-img" src="./images/select-car.png"></Image>
+                    <h1>Deine <br />Label-Übersicht</h1>
+                </Card.Title>
+                <Card.Body className="card-body" style={{ width: '18rem' }}>
+                    <div className="btn-area">
+                        {labels.map((label) => (
+                            <div className={"display-row-flex"}  key={label.id}>
+                                <Link to={"/editLabel/" + label.id} className="btn-a-standard expand" key={label.id}> {label.name}</Link> <Button onClick={() =>deleteLabel(label.id)}>Löschen</Button>
+                            </div>
+                        ))}
+                        <Link to={"/addLabel"} className="btn-a-standard ">Label hinzufügen</Link>
+                    </div>
+                </Card.Body>
+                <Footer />
+            </Card>)}
+            <p>LABEL OVERVIEW</p>
+        </>
+    );
+}
