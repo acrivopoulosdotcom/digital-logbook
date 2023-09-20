7b38924e60ae3a53951947c773dfc7a703944c5f
clenn <acrivopoulosdotcom@gmail.com>
2023-09-20 18:08:24 +0200

    delete scroll effect

--- frontend/src/Footer/Footer.tsx
+++ frontend/src/Footer/Footer.tsx
@@ -1,25 +1,14 @@
 import Container from "react-bootstrap/Container";
 import './Footer.css'
 import {Image} from "react-bootstrap";
-import {useState} from "react";
+
 
 
 export default function Footer() {
-    const [isHovered, setIsHovered] = useState(false);
-    const [isActive, setIsActive] = useState(false);
     return (
         <>
-            <Container className="footer"
-
-            >
-                <a href="#header"
-                   onMouseEnter={() => setIsHovered(true)}
-                   onMouseLeave={() => setIsHovered(false)}
-                   onTouchStart={() => setIsActive(true)}
-                   onTouchEnd={() => setIsActive(false)}
-                >
-                    <Image className="footer-icon" src={isHovered || isActive ? "./icons/red-card.png" : "./icons/blue-card.png"} rounded fluid />
-                </a>
+            <Container className="footer">
+                <Image className="footer-icon" src={"./icons/book-basic-icon.png"} fluid />
             </Container>
         </>
     )
