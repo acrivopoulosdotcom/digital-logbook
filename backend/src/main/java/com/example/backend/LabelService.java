f9e70228f6b2059600dd93bb1b49e5d3c34b9a0e
clenn <acrivopoulosdotcom@gmail.com>
2023-09-20 17:34:44 +0200

    finalize labelService

--- backend/src/main/java/com/example/backend/LabelService.java
+++ backend/src/main/java/com/example/backend/LabelService.java
@@ -3,7 +3,6 @@ package com.example.backend;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Data
@@ -14,9 +13,8 @@ public class LabelService {
     private final LabelRepository labelRepo;
     private final IdService idService;
 
-    public List<Label> getAllLabels() {return labelRepo.findAll();}
-
     public List<Label> getAllLabelsByUserId(String userId) {return labelRepo.findLabelsByUserId(userId);}
+
     public Label addLabel(Label label) {
         return labelRepo.save(label.withId(idService.generateID()));
     }
@@ -24,6 +22,4 @@ public class LabelService {
     public void deleteLabelByLabelId(String id) {
         labelRepo.deleteById(id);
     }
-
-
 }
