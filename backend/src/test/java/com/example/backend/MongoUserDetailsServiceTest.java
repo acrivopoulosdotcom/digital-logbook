029ea01f7871cb1669de3083ac90a6c670deb985
clenn <acrivopoulosdotcom@gmail.com>
2023-09-22 14:21:30 +0200

    implement tests

---
+++ backend/src/test/java/com/example/backend/MongoUserDetailsServiceTest.java
@@ -0,0 +1,17 @@
+package com.example.backend;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MongoUserDetailsServiceTest {
+
+    @Test
+    public void saveUser_() {
+    }
+
+    @Test
+    public void updateUser() {
+    }
+}
